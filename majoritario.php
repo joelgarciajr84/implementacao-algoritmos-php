51dee0392c1f8a0e37826215c1a8f68f79a480c1
Joel Garcia Jr <joel.garciajr84@gmail.com>
2014-07-13 02:44:43 -0300

    Alterações melhorias na implementação

--- majoritario.php
+++ majoritario.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 # Encontra um numero majoritario em um vetor.
 
-$votos = array(10,22,30,22,22,30,40,22,22,22);
+$votos = array(10,22,30,22,22,30,40,22,22,22,22);
 
 $totalvotos = count($votos);
 
@@ -14,16 +14,29 @@ $contagem = array_count_values($votos);
 
 $formula = $totalvotos / 2 + 1;
 
+$formula = floor($formula);
 
+echo "Para vencer são necessários ". $formula . " votos + 1" ."<br><br>";
 foreach ($contagem as $key => $repeticoes) {
 
 	if ($repeticoes >= $formula) {
 		
+		$vencedor = array($key, $repeticoes);
+	}else{
 
-		echo $key . " recebeu " . $repeticoes . " votos" . " e venceu as eleições";
+		if ($repeticoes > 1) {
+			echo $key . " recebeu " . $repeticoes . " votos <br> <br>";
+		}else{
+
+			echo $key . " recebeu " . $repeticoes . " voto <br> <br>";
+		}
 	}
 }
+if (isset($vencedor)) {
+		
+	echo $vencedor[0] ." recebeu ". $vencedor[1] . " votos e venceu!";
+}else{
 
-
-
+	echo "Não houve um vencedor!";
+}
  ?>
